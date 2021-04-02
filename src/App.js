9a2219f053d41b0d7d1c11e280d20b1e378f3fb3
paolorafaelyap <paolorafaelyap@gmail.com>
2021-04-02 13:59:08 -0700

    updated App.js

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+import Header from "./components/Header";
+import Table from "./components/Table";
+import TableHead from './components/TableHead'
+import Search from './components/Search'
+import people from "./people.json";
