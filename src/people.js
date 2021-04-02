621ba26a53a2b7a8c16a0792f2884f9c38fb9591
paolorafaelyap <paolorafaelyap@gmail.com>
2021-04-02 00:29:08 -0700

    added files

---
+++ src/people.js
@@ -0,0 +1,56 @@
+[
+    {
+      "id": 1,
+      "name": "Paolo Yap",
+      "role": "CEO",
+      "salary": 10000000000000,
+      "phone": "(707)334-5887",
+      "email": "paolorafaelyap@gmail.com",
+      "dob": "02-20-94"
+    },
+    {
+      "id": 2,
+      "name": "Doodle Bob",
+      "role": "CTO",
+      "salary": 1,
+      "phone": "(555)555-5555",
+      "email": "doodle@gmail.com",
+      "dob": "01-11-01"
+    },
+    {
+      "id": 3,
+      "name": "Police",
+      "role": "Security",
+      "salary": 75000,
+      "phone": "911",
+      "email": "weewoo@gmail.com",
+      "dob": "10-12-74"
+    },
+    {
+      "id": 4,
+      "name": "Spider Mann",
+      "role": "Photographer",
+      "salary": 100000,
+      "phone": "(532)313-4423",
+      "email": "thwipthwip@gmail.com",
+      "dob": "12-04-86"
+    },
+    {
+      "id": 5,
+      "name": "Patrick Star",
+      "role": "Philosopher",
+      "salary": 12345,
+      "phone": "(000)555-3123",
+      "email": "spongebobbff@gmail.com",
+      "dob": "02-28-68"
+    },
+    {
+      "id": 6,
+      "name": "Baba Booya",
+      "role": "Advertisement",
+      "salary": 70000,
+      "phone": "(222)542-3690",
+      "email": "bababooya@gmail.com",
+      "dob": "09-09-09"
+    }
+  ]
